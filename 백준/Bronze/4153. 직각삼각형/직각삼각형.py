70f8511590dc145502dc4f21d95063b24bb59680
Dohyeong Kim <dohyeong7026@gmail.com>
2023-09-07 17:21:12 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,14 @@
+import sys
+
+while True:
+    input = sys.stdin.readline()
+    data = list(map(int, input.split()))
+    if(data[0] == 0 and data[1] == 0 and data[2] == 0):
+        break
+
+    data.sort()
+
+    if data[0]**2 + data[1]**2 == data[2]**2:
+        print('right')
+    else:
+        print('wrong')
