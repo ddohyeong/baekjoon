930a00445a9d3150d177fa4ea071623dc32b42a7
Dohyeong Kim <dohyeong7026@gmail.com>
2024-07-20 14:30:32 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,24 +1,30 @@
-def fact(n):
-    data = [1]
+
+
+# def fact(n):
+#     data = [1]
     
-    sum = 1
-    for i in range(1, n+1):
-        sum *= i
-        data.append(sum)
+#     sum = 1
+#     for i in range(1, n+1):
+#         sum *= i
+#         data.append(sum)
     
-    return data
+#     return data
     
     
 
+# def solution(balls, share):
+#     answer = 0
+    
+#     data = fact(balls)
+#     print(data)
+    
+    
+#     answer = data[balls] // (data[balls - share] * data[share])
+    
+    
+    
+#     return answer
+import math
 def solution(balls, share):
     answer = 0
-    
-    data = fact(balls)
-    print(data)
-    
-    
-    answer = data[balls] // (data[balls - share] * data[share])
-    
-    
-    
-    return answer
+    return math.comb(balls, share)
