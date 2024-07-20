bb7479aa1af7f4d340718799c4d9719159cae966
Dohyeong Kim <dohyeong7026@gmail.com>
2024-07-20 14:00:54 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -0,0 +1,13 @@
+def solution(numbers, k):
+    answer = 0
+    
+    # 0,3,5,7,9,11
+    
+
+    data = (2 * k) -1
+    
+    return numbers[data % len(numbers) - 1]
+    
+    
+    
+    return answer
