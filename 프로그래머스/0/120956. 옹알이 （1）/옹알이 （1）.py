1a61e2b67e311cf6863c0deacf416bfd7415c98e
Dohyeong Kim <dohyeong7026@gmail.com>
2024-07-24 17:25:51 +0900

    [level 0] Title: 옹알이 (1), Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,22 @@
+
+
+def solution(babbling):
+    answer = 0
+    
+    data = ["aya", "ye", "woo", "ma"]
+    
+    for s in babbling:
+        tmp = s
+        for i in data:
+            s = s.replace(i, ' ')
+
+        s = s.replace(' ', '')    
+        
+        if s != '':
+            print(tmp)
+            continue
+        
+        answer += 1
+        
+    
+    return answer
