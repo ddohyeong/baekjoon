0606075639fd0d1e338f2fbe758594dfe70f9964
Dohyeong Kim <dohyeong7026@gmail.com>
2024-08-01 22:49:41 +0900

    [Silver III] Title: 칸토어 집합, Time: 48 ms, Memory: 32156 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,30 +1,11 @@
-import sys
-input = sys.stdin.readline
+ans = [' ' for i in range(13)]
+ans[0] = '-'
 
-def fun1(line):
-	if len(line) == 1:
-		return '-'
-	
-	slice_line = len(line) // 3
-
-	return fun1(line[0:slice_line]) + ' ' * slice_line  + fun1(line[(slice_line*2):])
-
-data = []
+for i in range(1, 13):
+	ans[i] = ans[i-1] + ' ' * (3 ** (i-1)) + ans[i -1]
 
 while True:
-
-	line = input()
-	if line == "":
+	try:
+		print(ans[int(input())])
+	except:
 		break
-	data.append(int(line))
-
-for N in data:
-
-	if N == 0:
-		print('-')
-		continue
-
-	ans = '-' * (3 ** N)
-
-	print(fun1(ans))
-
