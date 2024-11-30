b47b4ac733a240f3edd7bbc02359742011740958
Dohyeong Kim <dohyeong7026@gmail.com>
2024-12-01 02:24:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 6460 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,24 +1,19 @@
-import sys
-input = sys.stdin.readline
+N, K = map(int, input().split())
 
-N,K = map(int,input().split())
+W, V = [0], [0]
 
-data = []
-data.append([0,0])
 for _ in range(N):
-	data.append(list(map(int,input().split())))
+	w,v = map(int, input().split())
 
-dp =[[ 0 for _ in range(K+1)] for _ in range(N+1)]
+	W.append(w)
+	V.append(v)
 
+dp = [[0] * (K+1) for _ in range(N + 1)]
 
-
-for i in range(1,N+1):
-	for j in range(1, K+1):
-		
-		dp[i][j] = dp[i-1][j]
-		
-		if j - data[i][0] >= 0:
-			dp[i][j] = max(dp[i-1][j], dp[i-1][j - data[i][0]] + data[i][1])
-
+for n in range(1,N+1):
+	for k in range(1,K+1):
+		dp[n][k] = dp[n-1][k]
+		if k - W[n] >= 0:
+			dp[n][k] = max(dp[n-1][k], dp[n-1][k-W[n]] + V[n])
 
 print(dp[N][K])
