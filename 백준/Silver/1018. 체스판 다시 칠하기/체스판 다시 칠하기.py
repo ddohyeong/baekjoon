f11bb8d7ff0d5ea3cc391d794b40d1b6cd7eb3eb
Dohyeong Kim <dohyeong7026@gmail.com>
2024-12-04 20:11:05 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 2244 ms, Memory: 31716 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,36 +1,41 @@
-import sys
-input = sys.stdin.readline
-
+import copy
 N, M = map(int, input().split())
+data = [list(input()) for _ in range(N)]
 
-board = []
+t1 = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+t2 = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
 
-for _ in range(N):
-    board.append(input())
 
-# print(board)
-result = []
+check = []
+for i in range(N-8+1):
+	for j in range(M-8+1):
+		temp = []
 
-for i in range(N-7):
-    for j in range(M-7):
-        draw1 = 0
-        draw2 = 0
-        
-        for a in range(i, i+8):
-            for b in range(j, j+8):
-                if (a+b) % 2 == 0:
-                    if board[a][b] != 'B':
-                        draw1 +=1
-                    else:
-                        draw2 +=1
-                else:
-                    if board[a][b] != 'W':
-                        draw1 +=1
-                    else:
-                        draw2 +=1
-        
-        result.append(draw1)
-        result.append(draw2)
+		te1 = copy.deepcopy(data)
+		te2 = copy.deepcopy(data)
+		cnt1 = 0
+		cnt2 = 0
+		for x in range(i,i+8):
+			for y in range(j,j+8):
+				if te1[x][y] != t1[x-i][y-j]:
+					if te1[x][y] == 'B':
+						te1[x][y] = 'W'
+					else:
+						te1[x][y] = 'B'
+					cnt1 += 1
+				if te2[x][y] != t2[x-i][y-j]:
+					if te2[x][y] == 'B':
+						te2[x][y] = 'W'
+					else:
+						te2[x][y] = 'B'
+					cnt2 += 1
+	
+		check.append(min(cnt1, cnt2))
 
-print(min(result))
-        
+print(min(check))
+
+
+						
+
+
+
